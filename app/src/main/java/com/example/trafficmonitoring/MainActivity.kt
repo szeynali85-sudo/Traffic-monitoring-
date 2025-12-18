13d88618e1b1c6cf2f211c55bf649e63a46cbd9d
szeynali85-sudo <s.zeynali85@gmail.com>
2025-12-18 17:43:04 +0330

    Update MainActivity.kt

--- app/src/main/java/com/example/trafficmonitoring/MainActivity.kt
+++ app/src/main/java/com/example/trafficmonitoring/MainActivity.kt
@@ -1,1 +1,22 @@
+package com.example.trafficmonitoring
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface {
+                    Text("Traffic Monitoring App")
+                }
+            }
+        }
+    }
+}
 
